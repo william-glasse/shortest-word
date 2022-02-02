5055e7d56c132d84c9fe7e2192c52fd4eb9b749c
William Glasse <william.glasse@ibm.com>
2022-02-02 09:38:11 +0000

    fix: works for word sizes of greater than 10

--- src/shortestWord.js
+++ src/shortestWord.js
@@ -1,2 +1,7 @@
-const shortestWord = (word) => (word ? word.length : null);
+const shortestWord = (words) => {
+  const splitWord = words.split(' ');
+  const lengthOfWords = splitWord.map((word) => word.length);
+  return Math.min(...lengthOfWords);
+};
+
 module.exports = shortestWord;
--- tests/shortestWord.test.js
+++ tests/shortestWord.test.js
@@ -5,6 +5,15 @@ describe('Shortest Word', () => {
     expect(shortestWord('word')).toBe(4);
   });
   test('if the input is an empty string, return null', () => {
-    expect(shortestWord('')).toBe(null);
+    expect(shortestWord('')).toBe(0);
+  });
+  test('should return the length of the shortest word for a list of words', () => {
+    expect(shortestWord('bitcoin take')).toBe(4);
+    expect(
+      shortestWord('bitcoin take over the world maybe who knows perhaps'),
+    ).toBe(3);
+  });
+  test('should work for long words', () => {
+    expect(shortestWord('bitcoinerereere take')).toBe(4);
   });
 });
